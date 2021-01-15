eb091b3f3be082a96c2ee53bea6a21485676d2ac
KseniaVaksina <66374121+KseniaVaksina@users.noreply.github.com>
2021-01-15 10:04:06 +0300

    Update CHSN.py

--- CHSN.py
+++ CHSN.py
@@ -16,7 +16,7 @@ def startJoin(message):
         joinedFile.write(str(message.chat.id) + "\n")
         joinedUsers.add(message.chat.id)
 
-@bot.message_handler(commands=['spesial'])
+@bot.message_handler(commands=['special'])
 def mess(message):
     for user in joinedUsers:
         bot.send_message(user, message.text[message.text.find(' '):])
