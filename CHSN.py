f5845cc5d6a5cc3a4c3300064add04156fa209b9
KseniaVaksina <66374121+KseniaVaksina@users.noreply.github.com>
2021-01-24 16:16:05 +0300

    Update CHSN.py

--- CHSN.py
+++ CHSN.py
@@ -19,6 +19,64 @@ def startJoin(message):
     bot.send_message(message.from_user.id, var)
     print(var)
         
+        
+@bot.message_handler(commands=['students'])
+def get_op(message):
+      keyboard = types.InlineKeyboardMarkup()
+      key_pol = types.InlineKeyboardButton(text='Политология', callback_data='pol')
+      key_soc = types.InlineKeyboardButton(text='Cоциология', callback_data='soc')
+      key_gmu = types.InlineKeyboardButton(text='ГМУ', callback_data='gmu')
+      key_psy = types.InlineKeyboardButton(text='Психология', callback_data='psy')
+      key_lyc = types.InlineKeyboardButton(text='Лицей НИУ ВШЭ', callback_data='lyc')
+      keyboard.add(key_pol, key_soc,key_psy,key_gmu, key_lyc)
+      question = 'Выберите образовательную программу'
+      bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == 'pol':
+        var = 'По всем интересующим вас вопросам об ОП «Политология» вы можете обратиться к этим студентам: \n' \
+              'Георгий Комендантов, 2 курс – https://vk.com/zzimablue\n' \
+              'Елена Алыбина, 2 курс – https://vk.com/elenaalybina\n' \
+              'Артур Арзуманян, 2 курс – https://vk.com/artshic\n' \
+              'Георгий Шишкин, 3 курс – https://vk.com/goshishsh \n' \
+              'Варя Филиппова, 3 курс – https://vk.com/sadwwitch'
+    elif call.data == 'soc':
+        var = 'По всем интересующим вас вопросам об ОП «Социология» вы можете обратиться к этим студентам: \n' \
+              'Лариса Преображенская, 1 курс – https://vk.com/plutotlavie \n' \
+              'Дмитрий Максименко, 1 курс – https://vk.com/maximenko_dm \n' \
+              'Артём Колодяжный, 2 курс – https://vk.com/id_tomas_rock \n' \
+              'Чен Сун Хи (Анастасия Ли), 2 курс – https://vk.com/nastya_chen \n' \
+              'Елизавета Толстогузова, 2 курс – https://vk.com/lizok.keepsintouch \n' \
+              'Маргарита Жданкина, 3 курс – https://vk.com/mamiezh \n' \
+              'Ксения Ваксина, 3 курс – https://vk.com/id415749948 '
+    elif call.data == 'gmu':
+        var = 'По всем интересующим вас вопросам об ОП «ГМУ» вы можете обратиться к этим студентам: \n' \
+              'Дарья Дергачева, 2 курс – https://vk.com/d.dergacheva \n' \
+              'Алан Гадзаонов, 2 курс – https://vk.com/alangadzaonov \n' \
+              'Анастасия Правосуд, 2 курс – https://vk.com/pravosudnastya \n' \
+              'Илья Кузьмин, 2 курс – https://vk.com/existancevk \n' \
+              'Даниил Ползиков, 3 курс – https://vk.com/dankaplz \n' \
+              'Артём Шаюк, выпускник бакалавриата – https://vk.com/ashayuk'
+    elif call.data == 'psy':
+        var = 'По всем интересующим вас вопросам об ОП «Психология» вы можете обратиться к этим студентам: \n' \
+              'Ксения Хлебникова, 2 курс – https://vk.com/id273613605 \n' \
+              'Алина Шевелева, 2 курс – https://vk.com/sergeevnaa__a \n' \
+              'Салават Юлушев, 2 курс – https://vk.com/yulushevs \n' \
+              'Дария Шипова, 2 курс – https://vk.com/thisis_mydesign \n' \
+              'Николай Купцов, 2 курс – https://vk.com/id152807979 \n' \
+              'Александра Рожанская, 2 курс – https://vk.com/alex_the_witch \n' \
+              'Василиса Грабарь, 2 курс – https://vk.com/vgraba \n' \
+              'Софья Захарова, 2 курс – https://vk.com/ssssoncha \n' \
+              'Алёна Харитонова, 3 курс – https://vk.com/alykharit'
+    elif call.data == 'lyc':
+        var = 'По всем интересующим вас вопросам о Лицее НИУ ВШЭ, в том числе и организационным моментам,' \
+              ' вы можете обратиться к:\n' \
+              'Дмитрий Максименко, выпускник Лицея – https://vk.com/maximenko_dm \n' \
+              'А также вся актуальная информация о Лицее – https://vk.com/hsemeetingarea'
+    bot.send_message(call.message.chat.id, var, disable_web_page_preview = True)
+        
+        
 
 @bot.message_handler(commands=['special'])
 def mess(message):
@@ -32,7 +90,8 @@ def process_help(message):
           "/timetable – ссылка на расписание \n" \
           "/link – ссылка на все онлайн-мероприятия \n" \
           "/social_net – ссылки на основные социальные сети  \n" \
-          "/contacts – основные контакты организаторов" 
+          "/contacts – основные контакты организаторов \n" \
+          "/students - контакты студентов, у которых вы можете уточнить любые вопросы о Вышке и ФСН"
     bot.send_message(message.from_user.id, var)
     print(var)
 
@@ -54,12 +113,6 @@ def process_help(message):
           "Анастасия Ли – глава PR. +7 (914) 742-97-05. https://vk.com/nastya_chen \n" \
           "Дмитрий Пикулькин – глава технического сопровождения. +7 (983) 518-60-25 https://vk.com/id553263075 "
     bot.send_message(message.from_user.id, var, disable_web_page_preview = True)
-    print(var)
-
-@bot.message_handler(commands=['students'])
-def process_help(message):
-    var = "Вы можете узнать подробнее о жизни Вышки и факультета социальных наук у этих людей:"
-    bot.send_message(message.from_user.id, var)
     print(var)
     
     
